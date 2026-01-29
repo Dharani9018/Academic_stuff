3bcb6451a48e0d7fa0203f861e6dfe756b59f3bd
Dharani9018 <anu293575@gmail.com>
2026-01-29 18:55:43 +0530

    mpca week3

---
+++ sem4/MPCA/lab/week2/assign1b.s
@@ -0,0 +1,17 @@
+.Data
+	A: .WORD 1,2,3,4,5,6,7,8,9,10
+	SUM: .WORD 0
+.Text
+	LDR R1,=A
+	LDR R2,=SUM
+	MOV R4,#0
+	LDR R4,[R1]
+	MOV R5,#1
+	LOOP1:	LDR R3,[R1, #8]! 
+		ADD R4,R4,R3
+		ADD R5,R5,#1
+		CMP R5,#5
+		BNE LOOP1
+	STR R4,[R2]
+	SWI 0x011
+
