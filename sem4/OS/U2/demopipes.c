981fe96f5bd74891b42b1f36e81e31547d541f42
Dharani9018 <anu293575@gmail.com>
2026-02-09 17:56:36 +0530

    pipessssss

---
+++ sem4/OS/U2/demopipes.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+int main()
+{
+  char write_msg[BUFFER_SIZE] = "Message from parent";
+  char read_msg[BUFFER_SIZE] = "";
+  int fd[2]; //file descriptor
+  pid_t pid;
+
+  //create a pipe
+  
+  if(pipe(fd)==-1)
+  {
+    printf("Pipe failed\n");
+    return 1;
+  }
+
+  //fork a child process
+  pid = fork();
+
+  if(pid > 0)
+  {
+    close(fd[READ_END]);
+    write(fd[WRITE_END], write_msg,strlen(write_msg)+1);
+    close(fd[WRITE_END]);
+    wait(NULL);
+  }
+  else if(pid==0)
+  {
+    close(fd[WRITE_END]);
+
+    read(fd[READ_END],read_msg,BUFFER_SIZE);
+
+    printf("Child process recieves-> %s\n",read_msg);
+
+    close(fd[READ_END]);
+  }
+
+  else
+  {
+    printf("Fork Failed\n");
+    return 1;
+  }
+  return 0;
+
+}
